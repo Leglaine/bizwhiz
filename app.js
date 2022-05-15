317bee43d1d6431246a19cc3dd258c5f499f8c3e
Lexi Wright <ehdubya@hotmail.com>
2022-05-14 18:56:40 -0700

    Configure morgan

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+// Log all requests in combined format
+app.use(morgan("combined"));
 
 module.exports = app;
