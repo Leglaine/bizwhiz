ecde028863ac8c19bf04a48e1ec6f529b2f49e22
Lexi Wright <ehdubya@hotmail.com>
2022-05-15 01:37:54 -0700

    Disable morgan in test environment and add access-tokens router

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const accessTokensRouter = require("./api/routers/access-tokens.router");
 const usersRouter = require("./api/routers/users.router");
 
 const app = express();
 
 // Log all incoming HTTP/HTTPS requests in combined format
-app.use(morgan("combined"));
+if (process.env.NODE_ENV !== "test") {
+    app.use(morgan("combined"));
+}
 
 // Allow express to parse json requests
 app.use(express.json());
 
+app.use("/api/access-tokens", accessTokensRouter);
 app.use("/api/users", usersRouter);
 
 app.use((err, _req, res, _next) => {
