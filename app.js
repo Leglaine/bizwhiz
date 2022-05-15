06a746f1d984f9012b54c90f72340bad7d5feae3
Lexi Wright <ehdubya@hotmail.com>
2022-05-14 23:43:38 -0700

    Create error handler

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const usersRouter = require("./api/routers/users.router");
 
 const app = express();
 
-// Log all requests in combined format
+// Log all incoming HTTP/HTTPS requests in combined format
 app.use(morgan("combined"));
 
 // Allow express to parse json requests
 app.use(express.json());
 
 app.use("/api/users", usersRouter);
+
+app.use((err, _req, res, _next) => {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+});
 
 module.exports = app;
