4a782d137dc6a0b402bb06558f9d5bb8ab38f4c2
Lexi Wright <ehdubya@hotmail.com>
2022-05-14 20:03:47 -0700

    Configure test database

--- config/sequelize.js
+++ config/sequelize.js
@@ -7,5 +7,12 @@ module.exports = {
         database: "bizwhiz-dev",
         host: "localhost",
         dialect: "postgres"
+    },
+    test: {
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: "bizwhiz-test",
+        host: "localhost",
+        dialect: "postgres"
     }
 };
