9dd790870fe4c1640874bf38965946412ac1e90d
Lexi Wright <ehdubya@hotmail.com>
2022-05-14 20:03:30 -0700

    Add users router to app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const usersRouter = require("./api/routers/users.router");
+
 const app = express();
 
 // Log all requests in combined format
 app.use(morgan("combined"));
+
+app.use("/api/users", usersRouter);
 
 module.exports = app;
