ba65b5df2ee590585a1134ded9d0cfd85aae0dcc
Lexi Wright <ehdubya@hotmail.com>
2022-05-14 21:49:20 -0700

    Allow express to parse json requests

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 // Log all requests in combined format
 app.use(morgan("combined"));
 
+// Allow express to parse json requests
+app.use(express.json());
+
 app.use("/api/users", usersRouter);
 
 module.exports = app;
