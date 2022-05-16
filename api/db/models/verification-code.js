9001165516ae963f59f01ab7c87330fc3d8ece55
Lexi Wright <ehdubya@hotmail.com>
2022-05-16 01:48:36 -0700

    Create verification code model and migration

---
+++ api/db/models/verification-code.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class VerificationCode extends Model {
+        static associate(models) {
+            // define associations here
+        }
+    }
+    VerificationCode.init(
+        {
+            user_id: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                primaryKey: true
+            },
+            code: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true
+            }
+        },
+        {
+            sequelize,
+            modelName: "VerificationCode",
+            tableName: "verification_codes",
+            underscored: true
+        }
+    );
+    return VerificationCode;
+};
